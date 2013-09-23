9fc28ecd55d8f908b21d8af980e17474ec5c054d
marcellCsiszar <contact@csiszarmarcell.hu>
2013-09-23 23:53:20 +0200

    Corrected gitignore

--- app/controllers/uploader/dashboard_controller.rb
+++
@@ -1,9 +0,0 @@
-class Uploader::DashboardController < ApplicationController
-	before_filter :authenticate_uploader!
-  def index
-  	#Checking Getting Started page
-  	if current_user.last_sign_in_at.nil? then
-  		redirect_to :controller => :DashboardController, :action => :welcome
-	end
-  end
-end
